46b7b080d42146f4c67c0e372074b8eb21108363
plaflin <patricklaflin@gmail.com>
2019-07-29 10:05:42 -0400

    Initial Commit
    Initial Commit

---
+++ Virus/src/main/java/com/patricklaflin/Virus.java
@@ -0,0 +1,13 @@
+package com.patricklaflin;
+
+public class Virus {
+    static int virusCount = 0;
+
+    public Virus() {
+        virusCount++;
+    }
+
+    static int getVirusCount() {
+        return virusCount;
+    }
+}
